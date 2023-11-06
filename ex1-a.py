915b1c43f07838f28cfe1e3092e7b35b3caa5e4b
felixgetaccess <124807113+felixgetaccess@users.noreply.github.com>
2023-11-06 08:39:44 +0100

    Exercice 1 terminé
    Exercice 1 terminé

---
+++ ex1-a.py
@@ -0,0 +1,7 @@
+nom_entreprise = "Entreprise"
+nombre_clients = 100
+revenu_moyen_par_client = 500
+
+revenu_total = nombre_clients * revenu_moyen_par_client
+
+print(f"Le revenu total de {nom_entreprise} est de {revenu_total} euros.")
