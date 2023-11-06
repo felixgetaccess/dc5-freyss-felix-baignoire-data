1934b343080adb2486291535b8c6fb8a947cd500
felixgetaccess <124807113+felixgetaccess@users.noreply.github.com>
2023-11-06 09:00:39 +0100

    Upload exo2-a
    Upload exo2-a

---
+++ ex2-a.py
@@ -0,0 +1,8 @@
+depenses_mensuelles = [1000, 1200, 800, 1500, 900, 1100, 1300, 1000, 1200, 800, 1500, 900]
+
+depenses_annuelles = 0
+
+for depense in depenses_mensuelles:
+    depenses_annuelles += depense
+
+print(f"Les dépenses marketing annuelles sont de {depenses_annuelles} euros.")
