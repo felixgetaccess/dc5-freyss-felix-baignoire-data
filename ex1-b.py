7939529ebca8f04686cdb28eb38ef3aab2fb7f50
felixgetaccess <124807113+felixgetaccess@users.noreply.github.com>
2023-11-06 08:59:31 +0100

    correction exo1-b sans utiliser les fonctions

--- ex1-b.py
+++ ex1-b.py
@@ -1,12 +1,26 @@
 phrase_utilisateur = input("Veuillez saisir une phrase : ")
 
-phrase_majuscules = phrase_utilisateur.upper()
-phrase_minuscules = phrase_utilisateur.lower()
+phrase_majuscules = ""
+phrase_minuscules = ""
+
+for caractere in phrase_utilisateur:
+    if caractere.isalpha():
+        phrase_majuscules += caractere.upper()
+        phrase_minuscules += caractere.lower()
+    else:
+        phrase_majuscules += caractere
+        phrase_minuscules += caractere
+
+mot = False
+nombre_mots = 0
+
+for caractere in phrase_utilisateur:
+    if caractere.isalpha() and not mot:
+        mot = True
+        nombre_mots += 1
+    elif not caractere.isalpha():
+        mot = False
 
 print(f"La phrase en majuscules : {phrase_majuscules}")
 print(f"La phrase en minuscules : {phrase_minuscules}")
-
-mots = phrase_utilisateur.split()
-nombre_mots = len(mots)
-
 print(f"Le nombre de mots dans la phrase : {nombre_mots}")
